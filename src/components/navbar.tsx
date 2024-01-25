14888158042a538e5fdbf51f912ec291da48ed67
sikandertariq <clashersting@gmail.com>
2024-01-26 03:09:43 +0500

    Remove unused files and add react-router-dom dependency

---
+++ src/components/navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/navbar.css';
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="navbar ">
+      <div className="navbar-brand ">Your Brand</div>
+      <ul className="navbar-nav">
+        <li>
+          <Link to="/" className="navbar-link">Home</Link>
+        </li>
+        <li>
+          <Link to="/login" className="navbar-link">Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/login.tsx
@@ -0,0 +1,9 @@
+//import { Link } from "react-router-dom";
+export const Login = () => {
+    return (
+        <div>
+        <h1>Login Page</h1>
+        {/* <Link to="/"></Link> */}
+        </div>
+    );
+    }
