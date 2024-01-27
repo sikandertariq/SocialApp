b4ed8debfba80882a9974068c423e4a9eb44cd0b
Sikander Ali Tariq <clashersting@gmail.com>
2024-01-28 01:12:23 +0500

    unga bunga

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,24 +1,30 @@
-//import { Link } from "react-router-dom";
-import {auth, provider} from "../config/firebase";
-import {signInWithPopup} from "firebase/auth";
+// Login.tsx
+
+import React from "react";
+import { auth, provider } from "../config/firebase";
+import { signInWithPopup } from "firebase/auth";
+import "../css/login.css"; // Import the CSS file
+
 export const Login = () => {
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
-    const signInWithGoogle = () => {
-        signInWithPopup(auth, provider).then((result) => {
-            console.log(result);
-
-        }
-        ).catch((error) => {
-            console.log(error.message);
-
-    });
-    };
-        
-
-    return (
-        <div>
-        <h1>Login Page</h1>
-        <button onClick={signInWithGoogle}>Sign in with google</button>
-        </div>
-    );
-}
+  return (
+    <div className="items-center justify-center m-12 flex">
+      <h1 className="">Login Page</h1>
+      <button
+        className="m-12 "
+        onClick={signInWithGoogle}
+      >
+        Sign in with Google
+      </button>
+    </div>
+  );
+};
